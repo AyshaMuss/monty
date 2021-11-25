4eef99631b9564bf43707f697da44644e5416e28
AyshaMuss <Ayshamuss@gmail.com>
2021-11-25 22:54:52 +0300

    updates on SWAP

--- aux_functions.c
+++ aux_functions.c
@@ -117,7 +117,7 @@ void swap(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		free(stack);
 		var_glob[1] = 1;
-		exit(EXIT_FAILURE);
+		return;
 	}
 	j = (*stack)->n;
 	(*stack)->n = (*stack)->next->n;
